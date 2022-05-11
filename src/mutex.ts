fcb2ae2ce138ffc7a526b48e517eaad2d2260ff0
WatchDG <baranov@w3m.co>
2022-05-11 05:25:15 +0300

    upd

--- src/mutex.ts
+++ src/mutex.ts
@@ -1,5 +1,5 @@
 export class Mutex {
-    mutex: Uint8Array = new Uint8Array(new SharedArrayBuffer(1));
+    private readonly mutex: Uint8Array = new Uint8Array(new SharedArrayBuffer(1));
 
     acquire(): boolean {
         return Atomics.compareExchange(this.mutex, 0, 0, 1) == 0;
