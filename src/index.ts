ada65351f619307ea1778b12b0a6a111f7a3e5ad
WatchDG <baranov@w3m.co>
2022-05-26 08:58:54 +0300

    upd

--- src/index.ts
+++ src/index.ts
@@ -100,7 +100,7 @@ export class ClickhouseBuffer {
         const paths = files.map(function (filename) {
             return path.join(self.directoryPath, filename);
         });
-        const stream = filesToStream(Array.from(paths));
+        const stream = filesToStream(paths);
         await self.clickhouseClient.query({
             query: self.insertStatement,
             data: stream
--- tests/clickhouse-buffer.test.ts
+++ tests/clickhouse-buffer.test.ts
@@ -50,6 +50,8 @@ describe('clickhouse-buffer', function () {
             ctx.clickhouseBuffer.push([i]);
         }
 
+        await setTimeout(10);
+
         expect(ctx.clickhouseBuffer.rowsInMemory()).toBe(9);
         expect(ctx.clickhouseBuffer.fileNamesInMemory()).toBe(0);
 
@@ -60,6 +62,15 @@ describe('clickhouse-buffer', function () {
         expect(ctx.clickhouseBuffer.rowsInMemory()).toBe(0);
         expect(ctx.clickhouseBuffer.fileNamesInMemory()).toBe(1);
 
+        for (let i = 10; i <= 15; i++) {
+            ctx.clickhouseBuffer.push([i]);
+        }
+
+        await setTimeout(10);
+
+        expect(ctx.clickhouseBuffer.rowsInMemory()).toBe(6);
+        expect(ctx.clickhouseBuffer.fileNamesInMemory()).toBe(1);
+
         await setTimeout(6000);
 
         const result = await ctx.clickhouseBuffer.clickhouseClient.query(
@@ -68,7 +79,7 @@ describe('clickhouse-buffer', function () {
         );
         expect(result).toBeInstanceOf(Object);
         expect(result).toHaveProperty('rows');
-        expect(result.rows).toBe(10);
+        expect(result.rows).toBe(16);
         expect(result).toHaveProperty('data');
         expect(result.data).toEqual(
             expect.arrayContaining([
@@ -76,7 +87,10 @@ describe('clickhouse-buffer', function () {
                 { id: 2 }, { id: 3 },
                 { id: 4 }, { id: 5 },
                 { id: 6 }, { id: 7 },
-                { id: 8 }, { id: 9 }
+                { id: 8 }, { id: 9 },
+                { id: 10 }, { id: 11 },
+                { id: 12 }, { id: 13 },
+                { id: 14 }, { id: 15 }
             ])
         );
 
